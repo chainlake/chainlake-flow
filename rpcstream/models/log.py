ed76db64333ab14b9c6631bd4f274ac97013124d
Mike Liang <mike.liang@chainlake.io>
2026-04-09 05:40:58 +0000

    update evm schema

---
+++ rpcstream/models/log.py
@@ -0,0 +1,11 @@
+LOG_FIELDS = [
+    'chain_id',
+    'log_index',
+    'transaction_hash',
+    'transaction_index',
+    'block_hash',
+    'block_number',
+    'address',
+    'data',
+    'topics'
+]
